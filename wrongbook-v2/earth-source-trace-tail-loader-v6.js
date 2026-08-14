8bf84384aa1ab914495a85a9497bc6ec998e57b7
jamshih <dadada961229@gmail.com>
2026-08-14 12:36:33 +0800

    Add source trace tail loader

---
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -0,0 +1,3 @@
+// Source-trace tail loader. Keep page-specific implementations isolated and ordered.
+document.write('<link rel="stylesheet" href="./earth-source-trace-p250-v6.css"/>');
+document.write('<script src="./earth-source-trace-p250-v6.js"><\/script>');
