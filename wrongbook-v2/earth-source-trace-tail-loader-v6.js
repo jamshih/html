2c3bbb345015255345570d5f754ea1b6fffdf558
jamshih <dadada961229@gmail.com>
2026-08-14 12:43:43 +0800

    Load source trace page 251

--- wrongbook-v2/earth-source-trace-tail-loader-v6.js
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -1,3 +1,5 @@
 // Source-trace tail loader. Keep page-specific implementations isolated and ordered.
 document.write('<link rel="stylesheet" href="./earth-source-trace-p250-v6.css"/>');
 document.write('<script src="./earth-source-trace-p250-v6.js"><\/script>');
+document.write('<link rel="stylesheet" href="./earth-source-trace-p251-v6.css"/>');
+document.write('<script src="./earth-source-trace-p251-v6.js"><\/script>');
