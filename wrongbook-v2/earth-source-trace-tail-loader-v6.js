d5cf6d1e117ca487a45cb1663768368053f664de
jamshih <dadada961229@gmail.com>
2026-08-14 12:58:56 +0800

    Load page 252 detail overlay

--- wrongbook-v2/earth-source-trace-tail-loader-v6.js
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -5,3 +5,5 @@ document.write('<link rel="stylesheet" href="./earth-source-trace-p251-v6.css"/>
 document.write('<script src="./earth-source-trace-p251-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p252-v6.css"/>');
 document.write('<script src="./earth-source-trace-p252-v6.js"><\/script>');
+document.write('<link rel="stylesheet" href="./earth-source-trace-p252-detail-v6.css"/>');
+document.write('<script src="./earth-source-trace-p252-detail-v6.js"><\/script>');
