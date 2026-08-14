aff1de2ea9fbf09292637ac9ffd8804c757aa8f0
jamshih <dadada961229@gmail.com>
2026-08-14 20:41:37 +0800

    Remove duplicated p252 detail layer from source tail loader

--- wrongbook-v2/earth-source-trace-tail-loader-v6.js
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -5,8 +5,6 @@ document.write('<link rel="stylesheet" href="./earth-source-trace-p251-v6.css"/>
 document.write('<script src="./earth-source-trace-p251-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p252-v6.css"/>');
 document.write('<script src="./earth-source-trace-p252-v6.js"><\/script>');
-document.write('<link rel="stylesheet" href="./earth-source-trace-p252-detail-v6.css"/>');
-document.write('<script src="./earth-source-trace-p252-detail-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p253-v6.css"/>');
 document.write('<script src="./earth-source-trace-p253-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p253-correct-v6.css"/>');
