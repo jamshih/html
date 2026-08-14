e49633eddf3cf32f4ef6c2d6cbcbd8b83832b925
jamshih <dadada961229@gmail.com>
2026-08-14 13:13:19 +0800

    Load final page 253 correction

--- wrongbook-v2/earth-source-trace-tail-loader-v6.js
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -9,3 +9,5 @@ document.write('<link rel="stylesheet" href="./earth-source-trace-p252-detail-v6
 document.write('<script src="./earth-source-trace-p252-detail-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p253-v6.css"/>');
 document.write('<script src="./earth-source-trace-p253-v6.js"><\/script>');
+document.write('<link rel="stylesheet" href="./earth-source-trace-p253-correct-v6.css"/>');
+document.write('<script src="./earth-source-trace-p253-correct-v6.js"><\/script>');
