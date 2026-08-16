cb4f3a8d28aaa3bfd408775366be3d1e766cb07b
jamshih <dadada961229@gmail.com>
2026-08-16 10:26:02 +0800

    Match p242 three-cause source grouping

--- wrongbook-v2/earth-source-prompts-v7-hotfix.js
+++ wrongbook-v2/earth-source-prompts-v7-hotfix.js
@@ -3,6 +3,7 @@
  const M=window.SOURCE_PROMPTS_V7||{};
  const q=(page,n)=>M[page]?.find(x=>x.number===n);
  let r=q(242,17); if(r)r.template='<b class="v4strict-num">(17)</b> {{0}} 溫狀態';
+ r=q(242,18); if(r)r.template='<span class="v7-p242-cause-heading"><b class="v4strict-num">(18)</b><strong>3大主因</strong></span><span class="v7-p242-cause-row"><b>1.</b>{{0}}</span><span class="v7-p242-cause-row"><b>2.</b>{{1}}</span><span class="v7-p242-cause-row"><b>3.</b>{{2}}</span>';
  r=q(243,35); if(r){r.sourceAnswers=['母元素衰變為原來二分之一所需時間'];r.answerAliases=[['母元素數量衰變為原來一半所需時間']];}
  r=q(243,36); if(r)r.dropOldAlias=true;
  r=q(243,37); if(r)r.dropOldAlias=true;
