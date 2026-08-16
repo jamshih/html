0dd62517192bd27b3966f974ee424ef2667d960d
jamshih <dadada961229@gmail.com>
2026-08-16 16:01:00 +0800

    Switch Earth p253 from correction overlay to V9 final owner

--- wrongbook-v2/earth-source-trace-tail-loader-v6.js
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -7,5 +7,6 @@ document.write('<link rel="stylesheet" href="./earth-source-trace-p252-v6.css"/>
 document.write('<script src="./earth-source-trace-p252-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p253-v6.css"/>');
 document.write('<script src="./earth-source-trace-p253-v6.js"><\/script>');
-document.write('<link rel="stylesheet" href="./earth-source-trace-p253-correct-v6.css"/>');
-document.write('<script src="./earth-source-trace-p253-correct-v6.js"><\/script>');
+// V9 is deliberately late-booted after the complete V6/V7 prompt/refinement stack.
+// The old p253 correction overlay is no longer loaded: V9 owns the final p253 DOM and geometry.
+document.write('<script src="./earth-source-hierarchy-v9-loader.js"><\/script>');
