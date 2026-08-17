40408e9c9574d45709d8506e3da4244ca9d9e312
jamshih <dadada961229@gmail.com>
2026-08-17 10:09:43 +0800

    fix(wrongbook): load Earth cluster ownership layer after source pages

--- wrongbook-v2/earth-source-trace-tail-loader-v6.js
+++ wrongbook-v2/earth-source-trace-tail-loader-v6.js
@@ -9,3 +9,6 @@ document.write('<link rel="stylesheet" href="./earth-source-trace-p253-v6.css"/>
 document.write('<script src="./earth-source-trace-p253-v6.js"><\/script>');
 document.write('<link rel="stylesheet" href="./earth-source-trace-p253-correct-v6.css"/>');
 document.write('<script src="./earth-source-trace-p253-correct-v6.js"><\/script>');
+// v9 repairs only source-photo cluster ownership/geometry after all 12 page implementations exist.
+document.write('<link rel="stylesheet" href="./earth-cluster-layout-v9.css"/>');
+document.write('<script src="./earth-cluster-layout-v9.js"><\/script>');
